32b0b986cd118a377443196f1c4ea142506b9a10
Alex-YuYan <alex.yan@nyu.edu>
2022-12-07 19:44:03 -0500

    feat: add intro & background page skeleton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Background from "./Background";
 import Intro from "./Intro";
 
 function App() {
   return (
     <>
-     <Routes>
-      <Route path="/" element={<Intro />} />
-      <Route path="bg" element={<Background />}/>
-     </Routes>
+      {/* Nav Bar */}
+      <div class="w-full container mx-auto pt-5 pl-5">
+        <div class="w-full flex items-center justify-between">
+          <Link class="flex items-center text-indigo-300 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" to="/">
+            AI<span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">GO</span>
+          </Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="bg" element={<Background />} />
+      </Routes>
+      {/* Footer */}
+      <div className="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+        <span></span>
+      </div>
     </>
   );
 }
--- src/Background.js
+++ src/Background.js
@@ -1,7 +1,35 @@
 const Background = () => (
-  <div>
-      BG
-  </div>
+    <div className="h-full">
+      {/* Main */}
+      <div className="container mx-auto w-full items-center justify-center flex flex-wrap flex-col md:flex-row items-center">
+
+        {/* Text box */}
+        <div className="flex flex-col w-3/4 lg:items-start overflow-y-hidden">
+          <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+              Background
+            </span>
+          </h1>
+
+          <form className="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label className="block text-blue-300 py-2 font-bold mb-2" for="emailaddress">
+                TODO: Background text
+              </label>
+            </div>
+
+            <div className="flex items-center justify-between pt-4">
+              <button
+                className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                type="button"
+              >
+                Continue
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
 )
 
 export default Background
